8d1a67d3d5194c377b287d8efa93ec7439706bdd
Sujan Sriram <sujansriram9@gmail.com>
2023-05-04 14:46:37 +0100

    dealership class and methods created

---
+++ dealership.js
@@ -0,0 +1,30 @@
+const Dealership = function (name, maxStock){
+    this.name = name;
+    this.maxStock = maxStock;
+    this.currentStock = [];
+}
+
+Dealership.prototype.countStock = function(){
+    return this.currentStock.length;
+}
+
+Dealership.prototype.addCarToStock = function(car){
+    this.currentStock.push(car);
+}
+
+Dealership.prototype.manufacturers = function(){
+    const manufacturers = this.currentStock.map(car => car.manufacturer);
+    return manufacturers;
+}
+
+Dealership.prototype.getCarsByManufacturer = function(manufacturerName){
+    const carsByManufacturer = this.currentStock.map(car => car.manufacturer === manufacturerName);
+    return carsByManufacturer;
+}
+
+Dealership.prototype.totalStockValue = function(){
+    const totalValue = this.currentStock.reduce((accumulator, car) => accumulator + car.price,0);
+    return totalValue;
+}
+
+module.exports = Car;
